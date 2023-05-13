5d25f3f2b4683023b6b0b8224cc930c89394f375
sadohasldgh;lsadjgl;ksadg <evgenkoli8@gmail.com>
2023-05-13 23:12:28 +0300

    subjects presentation

--- idkf/src/app/app.routes.ts
+++ idkf/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import {Routes} from '@angular/router';
+import {SubjectsComponent} from "./presentation/subjects/subjects.component";
 
 export const appRoutes: Routes = [
     {path: '', redirectTo: '/login', pathMatch: 'full'},
     // { path: 'login', component: LoginPageComponent },
-    // { path: 'subjects', component: SubjectsPageComponent },
+    { path: 'subjects', component: SubjectsComponent }
     // { path: 'all-tasks', component: AllTasksPageComponent },
     // { path: 'tasks/:subjectId', component: TasksOfSubjectPageComponent },
 ];
