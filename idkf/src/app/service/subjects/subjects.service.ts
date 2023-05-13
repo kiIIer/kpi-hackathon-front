3ea5fe199941e4fd8a2af959cc6dd3893b156e94
killer <michael.molchanov.2004@gmail.com>
2023-05-13 22:01:19 +0200

    observe response in subjects service

--- idkf/src/app/service/subjects/subjects.service.ts
+++ idkf/src/app/service/subjects/subjects.service.ts
@@ -1,38 +1,40 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
 import {Subject} from '../../store/entities/subject/subject.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class SubjectService {
-  private baseUrl = 'http://localhost:8080/api/Subject';
+    private baseUrl = 'http://localhost:8080/api/Subject';
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+    }
 
-  getSubjectById(id: number): Observable<Subject> {
-    const url = `${this.baseUrl}/${id}`;
-    return this.http.get<Subject>(url);
-  }
+    getSubjectById(id: number): Observable<HttpResponse<Subject>> {
+        const url = `${this.baseUrl}/${id}`;
+        return this.http.get<Subject>(url, {observe: 'response'});
+    }
 
-  // Without tasks, will return new
-  updateSubjectById(id: number, updatedSubject: Subject): Observable<Subject> {
-    const url = `${this.baseUrl}/${id}`;
-    return this.http.put<Subject>(url, updatedSubject);
-  }
+    // Without tasks, will return new
+    updateSubjectById(id: number, updatedSubject: Subject): Observable<HttpResponse<Subject>> {
+        const url = `${this.baseUrl}/${id}`;
+        return this.http.put<Subject>(url, updatedSubject, {observe: 'response'});
+    }
 
-  deleteSubjectById(id: number): Observable<void> {
-    const url = `${this.baseUrl}/${id}`;
-    return this.http.delete<void>(url);
-  }
-  // Without tasks
-  getSubjects(): Observable<Subject[]> {
-    return this.http.get<Subject[]>(this.baseUrl);
-  }
+    deleteSubjectById(id: number): Observable<HttpResponse<void>> {
+        const url = `${this.baseUrl}/${id}`;
+        return this.http.delete<void>(url, {observe: 'response'});
+    }
 
-  // Without tasks
-  createSubject(newSubject: Subject): Observable<Subject> {
-    return this.http.post<Subject>(this.baseUrl, newSubject);
-  }
+    // Without tasks
+    getSubjects(): Observable<HttpResponse<Subject[]>> {
+        return this.http.get<Subject[]>(this.baseUrl, {observe: 'response'});
+    }
+
+    // Without tasks
+    createSubject(newSubject: Subject): Observable<HttpResponse<Subject>> {
+        return this.http.post<Subject>(this.baseUrl, newSubject, {observe: 'response'});
+    }
 }
