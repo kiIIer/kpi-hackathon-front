17c3a220d2fdec4f39655a8e557e451e29918c6d
killer <michael.molchanov.2004@gmail.com>
2023-05-13 21:35:55 +0200

    I DID IT

--- idkf/src/app/app.component.ts
+++ idkf/src/app/app.component.ts
@@ -2,14 +2,25 @@ import {Component} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MainPageComponent} from './presentation/main-page/main-page.component';
+import {MatButtonModule} from '@angular/material/button';
+import {TaskService} from './service/tasks/tasks.service';
+import {AuthService} from '@auth0/auth0-angular';
 
 @Component({
     standalone: true,
-    imports: [RouterModule, MatProgressSpinnerModule, MainPageComponent],
+    imports: [RouterModule, MatProgressSpinnerModule, MainPageComponent, MatButtonModule],
     selector: 'idkf-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
     title = 'idkf';
+
+    constructor(public auth: AuthService) {
+    }
+
+    printUserData() {
+        this.auth.user$.forEach((u) => console.log(u));
+        this.auth.getAccessTokenSilently().forEach((t)=>console.log(t))
+    }
 }
--- idkf/src/app/app.routes.ts
+++ idkf/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import {Routes} from '@angular/router';
 
 export const appRoutes: Routes = [
-    {path: '', redirectTo: '/login', pathMatch: 'full'},
+    // {path: '', redirectTo: '/login', pathMatch: 'full'},
     // { path: 'login', component: LoginPageComponent },
     // { path: 'subjects', component: SubjectsPageComponent },
     // { path: 'all-tasks', component: AllTasksPageComponent },
