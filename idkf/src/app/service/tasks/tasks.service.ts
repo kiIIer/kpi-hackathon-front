e4ed353c263b04c4b1dfc506833cc2b52a64ce7d
killer <michael.molchanov.2004@gmail.com>
2023-05-13 17:02:08 +0200

    full tasks service

--- idkf/src/app/service/tasks/tasks.service.ts
+++ idkf/src/app/service/tasks/tasks.service.ts
@@ -1,18 +1,37 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Subject} from '../../store/entities/subject/subject.model';
+import {Task} from '../../store/entities/task/task.model';
 
 @Injectable({
     providedIn: 'root',
 })
-export class TasksService {
-    private api_url = 'http://localhost:8080/subject';
+export class TaskService {
+    private baseUrl = 'http://localhost:8080/api/Task';
 
     constructor(private http: HttpClient) {
     }
 
-    getSubject(): Observable<Subject> {
-        return this.http.get<Subject>(this.api_url);
+    getTaskById(id: number): Observable<Task> {
+        const url = `${this.baseUrl}/${id}`;
+        return this.http.get<Task>(url);
+    }
+
+    updateTaskById(id: number, updatedTask: Task): Observable<Task> {
+        const url = `${this.baseUrl}/${id}`;
+        return this.http.put<Task>(url, updatedTask);
+    }
+
+    deleteTaskById(id: number): Observable<void> {
+        const url = `${this.baseUrl}/${id}`;
+        return this.http.delete<void>(url);
+    }
+
+    getTasks(): Observable<Task[]> {
+        return this.http.get<Task[]>(this.baseUrl);
+    }
+
+    createTask(newTask: Task): Observable<Task> {
+        return this.http.post<Task>(this.baseUrl, newTask);
     }
 }
