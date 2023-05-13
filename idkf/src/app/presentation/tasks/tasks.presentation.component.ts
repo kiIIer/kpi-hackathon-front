f6c249e8f7c42b6133cf59192142a5f63b1e6006
sadohasldgh;lsadjgl;ksadg <evgenkoli8@gmail.com>
2023-05-13 18:50:21 +0300

    flashy logic

--- idkf/src/app/presentation/tasks/tasks.presentation.component.ts
+++ idkf/src/app/presentation/tasks/tasks.presentation.component.ts
@@ -15,18 +15,18 @@ import {MatIconModule} from '@angular/material/icon';
 export class TasksPresentationComponent {
     @Input() tasks: Task[] = [{
         id: 1,
-        name: 'Do this task',
-        description: 'Its like, important',
-        deadline: '12/01',
+        name: 'Системне програмування',
+        description: 'Курсова робота',
+        deadline: '2023-05-15T20:20:28.349Z',
         maxGrade: 10,
         status: 1,
         subjectId: 1,
     },
         {
             id: 2,
-            name: 'Do this task as well',
+            name: 'Інженерія програмного забезпечення',
             description: 'Its like, not important',
-            deadline: '11/01',
+            deadline: '2023-05-13T15:12:28.349Z',
             maxGrade: 9,
             status: 0,
             subjectId: 1,
@@ -38,4 +38,19 @@ export class TasksPresentationComponent {
         {name: 'menu', class: 'yellow'},
         {name: 'check', class: 'green'},
     ];
+
+    isDeadlineApproaching(deadline: string): boolean {
+        const TWO_DAYS_IN_MS = 2 * 24 * 60 * 60 * 1000;
+        const deadlineTime = new Date(deadline).getTime();
+        const currentTime = Date.now();
+        const timeDiff = deadlineTime - currentTime;
+        return timeDiff <= TWO_DAYS_IN_MS;
+    }
+
+    getFormattedDate(deadline: string): string {
+        const taskDeadline = new Date(deadline);
+        const month = taskDeadline.getMonth() + 1;
+        const day = taskDeadline.getDate();
+        return `${month}/${day}`;
+    }
 }
