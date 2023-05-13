251aca3669ea3ae3d42955e98d926acad3c6279d
killer <michael.molchanov.2004@gmail.com>
2023-05-13 13:03:17 +0200

    added main page

--- idkf/src/app/app.component.ts
+++ idkf/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MainPageComponent} from './presentation/main-page/main-page.component';
 
 @Component({
     standalone: true,
-    imports: [RouterModule, MatProgressSpinnerModule],
+    imports: [RouterModule, MatProgressSpinnerModule, MainPageComponent],
     selector: 'idkf-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
