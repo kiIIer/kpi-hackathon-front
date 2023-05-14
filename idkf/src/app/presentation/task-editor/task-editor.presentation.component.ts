e2c50e29e3f3e6c2520321ab12eaa2152796ce4a
killer <michael.molchanov.2004@gmail.com>
2023-05-14 05:24:19 +0200

    generated presentation component

---
+++ idkf/src/app/presentation/task-editor/task-editor.presentation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'idkf-task-editor.presentation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './task-editor.presentation.component.html',
+  styleUrls: ['./task-editor.presentation.component.css'],
+})
+export class TaskEditorPresentationComponent {}
