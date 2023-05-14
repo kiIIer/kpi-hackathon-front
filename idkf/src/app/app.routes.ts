2cf1c85bcd628b0659ef3cfa51c8872e06515aaf
killer <michael.molchanov.2004@gmail.com>
2023-05-14 03:52:36 +0200

    do you know the way?

--- idkf/src/app/app.routes.ts
+++ idkf/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import {Routes} from '@angular/router';
 import {SubjectsPresentationComponent} from './presentation/subjects/subjects.presentation.component';
 import {AllTasksContainerComponent} from './container/all-tasks.container/all-tasks.container.component';
+import {SubjectsContainerComponent} from './container/subjects.container/subjects.container.component';
 
 export const appRoutes: Routes = [
     // {path: '', redirectTo: '/login', pathMatch: 'full'},
     // { path: 'login', component: LoginPageComponent },
-    {path: 'subjects', component: SubjectsPresentationComponent},
+    {path: 'subjects', component: SubjectsContainerComponent},
     {path: 'all-tasks', component: AllTasksContainerComponent},
     // { path: 'tasks/:subjectId', component: TasksOfSubjectPageComponent },
 ];
