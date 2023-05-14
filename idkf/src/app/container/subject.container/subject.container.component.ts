21be97c812a598c37e11c01ed8c0bc0aee992994
killer <michael.molchanov.2004@gmail.com>
2023-05-14 04:13:21 +0200

    and now we are using presentation component, yay

--- idkf/src/app/container/subject.container/subject.container.component.ts
+++ idkf/src/app/container/subject.container/subject.container.component.ts
@@ -4,11 +4,12 @@ import {Store} from '@ngrx/store';
 import {Observable} from 'rxjs';
 import {Task} from '../../store/entities/task/task.model';
 import {selectFilteredTasks, selectTasksBySubjectId} from '../../store/entities/task/task.reducer';
+import {TasksPresentationComponent} from '../../presentation/tasks/tasks.presentation.component';
 
 @Component({
     selector: 'idkf-subject-container',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, TasksPresentationComponent],
     templateUrl: './subject.container.component.html',
     styleUrls: ['./subject.container.component.css'],
 })
