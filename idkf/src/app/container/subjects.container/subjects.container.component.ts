fd6ec0511cbb2674f7a39b9b2b4780716ddea00f
killer <michael.molchanov.2004@gmail.com>
2023-05-14 03:50:25 +0200

    get the subjects

--- idkf/src/app/container/subjects.container/subjects.container.component.ts
+++ idkf/src/app/container/subjects.container/subjects.container.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Store } from '@ngrx/store';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Store} from '@ngrx/store';
+import {Subject} from '../../store/entities/subject/subject.model';
+import {Observable} from 'rxjs';
+import {selectAll} from '../../store/entities/subject/subject.reducer';
 
 @Component({
-  selector: 'idkf-subjects-container',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './subjects.container.component.html',
-  styleUrls: ['./subjects.container.component.css']
+    selector: 'idkf-subjects-container',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './subjects.container.component.html',
+    styleUrls: ['./subjects.container.component.css'],
 })
 export class SubjectsContainerComponent {
-  constructor(private store: Store) {}
+    subjects$: Observable<Subject[]>;
+
+    constructor(private store: Store) {
+        this.subjects$ = this.store.select(selectAll);
+    }
 }
