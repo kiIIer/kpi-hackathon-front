21806ec756cc0d2e6d080a5827422b0001e7cff4
kiIIer <39669501+kiIIer@users.noreply.github.com>
2023-05-14 07:25:45 +0200

    Auth state (#47)
    * actions and reducers for auth
    
    * added them to state
    
    * written effects
    
    * configed effects
    
    * added check auth dispatch

--- idkf/src/app/container/main.container/main.container.component.ts
+++ idkf/src/app/container/main.container/main.container.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Store } from '@ngrx/store';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Store} from '@ngrx/store';
 import {MainPageComponent} from '../../presentation/main-page/main-page.component';
+import {checkAuth} from '../../store/entities/auth/auth.actions';
 
 @Component({
-  selector: 'idkf-main-container',
-  standalone: true,
-  imports: [CommonModule, MainPageComponent],
-  templateUrl: './main.container.component.html',
-  styleUrls: ['./main.container.component.css']
+    selector: 'idkf-main-container',
+    standalone: true,
+    imports: [CommonModule, MainPageComponent],
+    templateUrl: './main.container.component.html',
+    styleUrls: ['./main.container.component.css'],
 })
 export class MainContainerComponent {
-  constructor(private store: Store) {}
+    constructor(private store: Store) {
+        this.store.dispatch(checkAuth());
+    }
 }
