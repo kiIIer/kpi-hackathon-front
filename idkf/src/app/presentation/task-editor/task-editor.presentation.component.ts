7920b04569f06b7951daa1e2fc42f9d09be52853
killer <michael.molchanov.2004@gmail.com>
2023-05-14 05:26:13 +0200

    made the basic setup of presentation

--- idkf/src/app/presentation/task-editor/task-editor.presentation.component.ts
+++ idkf/src/app/presentation/task-editor/task-editor.presentation.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Task} from '../../store/entities/task/task.model';
 
 @Component({
-  selector: 'idkf-task-editor.presentation',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './task-editor.presentation.component.html',
-  styleUrls: ['./task-editor.presentation.component.css'],
+    selector: 'idkf-task-editor-presentation',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './task-editor.presentation.component.html',
+    styleUrls: ['./task-editor.presentation.component.css'],
 })
-export class TaskEditorPresentationComponent {}
+export class TaskEditorPresentationComponent {
+    @Input() currentTask: Task | undefined | null;
+    @Output() submitEventer: EventEmitter<Task>;
+
+    constructor() {
+        this.submitEventer = new EventEmitter<Task>();
+    }
+}
