b0e9ff585124a25b6605be3e1534f4c90f41c6ea
killer <michael.molchanov.2004@gmail.com>
2023-05-14 04:03:25 +0200

    generated container for one subject

---
+++ idkf/src/app/container/subject.container/subject.container.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+
+@Component({
+  selector: 'idkf-subject.container',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './subject.container.component.html',
+  styleUrls: ['./subject.container.component.css']
+})
+export class SubjectContainerComponent {
+  constructor(private store: Store) {}
+}
