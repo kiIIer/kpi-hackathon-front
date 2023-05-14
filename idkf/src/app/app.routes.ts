7b82e99d2573ba5399fb6e0f18e9199b0a633f98
sadohasldgh;lsadjgl;ksadg <evgenkoli8@gmail.com>
2023-05-14 11:14:39 +0300

    styles for subject-editor

--- idkf/src/app/app.routes.ts
+++ idkf/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import {Routes} from '@angular/router';
-import {SubjectsPresentationComponent} from './presentation/subjects/subjects.presentation.component';
 import {AllTasksContainerComponent} from './container/all-tasks.container/all-tasks.container.component';
 import {SubjectsContainerComponent} from './container/subjects.container/subjects.container.component';
 import {SubjectContainerComponent} from './container/subject.container/subject.container.component';
