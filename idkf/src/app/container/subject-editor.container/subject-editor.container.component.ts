3eb651a139942d1dbc48e2f7cb6cc9946c1642ce
killer <michael.molchanov.2004@gmail.com>
2023-05-14 05:47:46 +0200

    added meat to publishing

--- idkf/src/app/container/subject-editor.container/subject-editor.container.component.ts
+++ idkf/src/app/container/subject-editor.container/subject-editor.container.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Store } from '@ngrx/store';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {Task} from '../../store/entities/task/task.model';
+import {selectCurrentTask} from '../../store/entities/task/task.reducer';
+import {updateTask} from '../../store/entities/task/task.actions';
+import {updateSubject} from '../../store/entities/subject/subject.actions';
+import {selectCurrentSubject} from '../../store/entities/subject/subject.reducer';
+import {Subject} from '../../store/entities/subject/subject.model';
 
 @Component({
-  selector: 'idkf-subject-editor-container',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './subject-editor.container.component.html',
-  styleUrls: ['./subject-editor.container.component.css']
+    selector: 'idkf-subject-editor-container',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './subject-editor.container.component.html',
+    styleUrls: ['./subject-editor.container.component.css'],
 })
 export class SubjectEditorContainerComponent {
-  constructor(private store: Store) {}
+
+    currentSubject$: Observable<Subject | undefined>;
+
+    constructor(private store: Store) {
+        this.currentSubject$ = this.store.select(selectCurrentSubject);
+    }
+
+    onSubmit(subject: Subject) {
+        this.store.dispatch(updateSubject({subject: subject}));
+    }
 }
