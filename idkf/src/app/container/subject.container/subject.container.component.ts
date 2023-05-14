618a9a5b46d517979e5d017dd2d72321b8ded168
killer <michael.molchanov.2004@gmail.com>
2023-05-14 04:03:48 +0200

    fixed selector name

--- idkf/src/app/container/subject.container/subject.container.component.ts
+++ idkf/src/app/container/subject.container/subject.container.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 
 @Component({
-  selector: 'idkf-subject.container',
+  selector: 'idkf-subject-container',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './subject.container.component.html',
