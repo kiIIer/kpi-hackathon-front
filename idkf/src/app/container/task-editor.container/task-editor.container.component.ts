2c03c27a9d9c635c506c43e3e30d061dd1dff553
killer <michael.molchanov.2004@gmail.com>
2023-05-14 05:00:55 +0200

    method to submit

--- idkf/src/app/container/task-editor.container/task-editor.container.component.ts
+++ idkf/src/app/container/task-editor.container/task-editor.container.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Store } from '@ngrx/store';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Store} from '@ngrx/store';
+import {Task} from '../../store/entities/task/task.model';
+import {updateTask} from '../../store/entities/task/task.actions';
 
 @Component({
-  selector: 'idkf-task-editor.container',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './task-editor.container.component.html',
-  styleUrls: ['./task-editor.container.component.css']
+    selector: 'idkf-task-editor.container',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './task-editor.container.component.html',
+    styleUrls: ['./task-editor.container.component.css'],
 })
 export class TaskEditorContainerComponent {
-  constructor(private store: Store) {}
+    constructor(private store: Store) {
+    }
+
+    onSubmit(task: Task) {
+        this.store.dispatch(updateTask({task: task}));
+    }
 }
