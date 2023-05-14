9752ba9f26ede2eb2b2f2837120265881239f7d4
killer <michael.molchanov.2004@gmail.com>
2023-05-14 03:51:30 +0200

    hooked up presentation compoonent

--- idkf/src/app/container/subjects.container/subjects.container.component.ts
+++ idkf/src/app/container/subjects.container/subjects.container.component.ts
@@ -4,11 +4,12 @@ import {Store} from '@ngrx/store';
 import {Subject} from '../../store/entities/subject/subject.model';
 import {Observable} from 'rxjs';
 import {selectAll} from '../../store/entities/subject/subject.reducer';
+import {SubjectsPresentationComponent} from '../../presentation/subjects/subjects.presentation.component';
 
 @Component({
     selector: 'idkf-subjects-container',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, SubjectsPresentationComponent],
     templateUrl: './subjects.container.component.html',
     styleUrls: ['./subjects.container.component.css'],
 })
