2ced12f8c71acad88c59f0f06082dd51d3351fcd
kiIIer <39669501+kiIIer@users.noreply.github.com>
2023-05-15 11:57:17 +0200

    Finilize readme (#67)
    * fixed sorting
    
    * screenshots
    
    * updated readme

--- idkf/src/app/container/all-tasks.container/all-tasks.container.component.ts
+++ idkf/src/app/container/all-tasks.container/all-tasks.container.component.ts
@@ -7,6 +7,7 @@ import {selectAll} from '../../store/entities/task/task.reducer';
 import {TasksPresentationComponent} from '../../presentation/tasks/tasks.presentation.component';
 import {goToUrl} from '../../store/router/router.action';
 import {deleteTask, updateTask} from '../../store/entities/task/task.actions';
+import {map} from 'rxjs/operators';
 
 @Component({
     selector: 'idkf-all-tasks-container',
@@ -18,16 +19,24 @@ import {deleteTask, updateTask} from '../../store/entities/task/task.actions';
 export class AllTasksContainerComponent {
     allTasks$: Observable<Task[]>;
 
-    update(task: Task){
-        this.store.dispatch(updateTask({task: task}))
+    update(task: Task) {
+        this.store.dispatch(updateTask({task: task}));
     }
 
     constructor(private store: Store) {
-        this.allTasks$ = this.store.select(selectAll);
+        this.allTasks$ = this.store.select(selectAll).pipe(
+            map((tasks) =>
+                tasks.sort((a, b) => this.getDateFromString(a.deadline).getTime() - this.getDateFromString(b.deadline).getTime()),
+            ),
+        );
     }
 
     go(url: string) {
         this.store.dispatch(goToUrl({url: url}));
+    }
+
+    getDateFromString(dateString: string): Date {
+        return new Date(dateString);
     }
 
     delete(task: Task) {
